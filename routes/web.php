88aa51654047e14cb59b94d0754327d766ed7b96
Cipto Hadi <ciptohadi79@gmail.com>
2017-10-31 02:13:04 +0000

    account and admin route is using the same controller

--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,10 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-$router->get('/admin/users/{id}', 'UserConroller@show');
-
-$router->get('/account/profile/{id}', 'UserConroller@show');
 
 
-// create seperate controller
-$router->get('/admin/users/{id}', 'UserConroller@show');
+$router->get('/account/profile/{id}', 'UserController@show');
 
-$router->get('/account/profile/{id}', 'AccountConroller@show');
+$router->get('/admin/users/{id}', 'UserController@show');
+
 
